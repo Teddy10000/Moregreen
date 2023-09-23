05bad4520803b2be85d8f632276380a43556ae71
Teddy10000 <tedakrong12@gmail.com>
2023-09-23 22:27:35 +0000

    added some authentication with next-auth and now coming to apply on the app

---
+++ frontend/moregreen/src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,48 @@
+import type { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import  CredentialsProvider  from "next-auth/providers/credentials";
+
+export const options: NextAuthOptions = {
+    providers: [
+        GoogleProvider({
+          clientId: process.env.GOOGLE_CLIENT_ID as string,
+          clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+          authorization: {
+            params: {
+              prompt: "consent",
+              access_type: "offline",
+              response_type: "code"
+            }
+          }
+        }),
+        CredentialsProvider({
+            name: "Credentials",
+            credentials:{
+                username:{
+                    label:"Username:",
+                    type:"text",
+                    placeholder:"your-username"
+                }, 
+
+                password:{
+                    label:"Password",
+                    type:"password",
+                    placeholder:"mine password"
+                }
+            },
+            async authorize(credentials) {
+                
+                
+                const user = {id:"52", name:"Teddy",password:"DFNKSDFNDS"}
+
+                if (credentials?.username === user.name && credentials?.password === user.password) {
+                    return user
+                }
+                else {
+                    return null
+                }
+            }
+        })
+      ],
+    
+}
