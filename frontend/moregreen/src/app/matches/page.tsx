fec55ecc3ccf17da5fa45685a125546ac424cf74
Teddy10000 <tedakrong12@gmail.com>
2023-09-26 12:14:07 +0000

    solved some more

--- frontend/moregreen/src/app/matches/page.tsx
+++ frontend/moregreen/src/app/matches/page.tsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import { useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
 
@@ -17,24 +17,6 @@ export default function ClientPage(){
             <h1 ></h1>
         </section>
     )
-=======
-import { useSession } from "next-auth/react";
-import { redirect } from "next/navigation";
 
 
-
-export default function ClientPage(){
-    const {data:session} = useSession({
-        required:true,
-        onUnauthenticated(){
-            redirect('/api/auth/signin?callbackUrl=/client')
-        }
-    })
-
-    return (
-        <section>
-            <h1 ></h1>
-        </section>
-    )
->>>>>>> 205af39edd9b980b34dc296111e7cfc7eb057a24
 }
