f6ecfd369e1c680b9973d577c50a94a0f3798bfd
Teddy10000 <tedakrong12@gmail.com>
2023-09-26 15:35:54 +0000

    done this

--- frontend/moregreen/next.config.js
+++ frontend/moregreen/next.config.js
@@ -1,25 +1,13 @@
-<<<<<<< HEAD
-/** @type {import('next').NextConfig} */
-<<<<<<< HEAD
-const nextConfig = {
-    images:{
-        domains: ['lh3.googleusercontent.com']
-    }
-}
-=======
-const nextConfig = {}
->>>>>>> frontend-setup
 
-module.exports = nextConfig
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-=======
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
     images:{
         domains: ['lh3.googleusercontent.com']
     }
 }
 
+
 module.exports = nextConfig
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
->>>>>>> 205af39edd9b980b34dc296111e7cfc7eb057a24
+
