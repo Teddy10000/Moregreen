72c166c756ef543a5859cb9558a95befa8e2e2a3
Teddy10000 <tedakrong12@gmail.com>
2023-09-26 15:33:59 +0000

    added this

--- frontend/moregreen/src/middleware.ts
+++ frontend/moregreen/src/middleware.ts
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
+
 export { default } from "next-auth/middleware"
 
 
 
 export const config = {
     matcher:["/extra","/predictions","/payment"]
-=======
-export { default } from "next-auth/middleware"
 
-
-
-export const config = {
-    matcher:["/extra","/predictions","/payment"]
->>>>>>> 205af39edd9b980b34dc296111e7cfc7eb057a24
 }
