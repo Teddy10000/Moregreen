e63ae9a6a2572a149193618c2e50173395a47d37
Teddy10000 <tedakrong12@gmail.com>
2023-09-26 12:09:38 +0000

    done

--- frontend/moregreen/sanity/sanity-utils.ts
+++ frontend/moregreen/sanity/sanity-utils.ts
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import {createClient,groq} from "next-sanity";
 
 export async function getProjects(){
@@ -30,35 +30,5 @@ export async function getProjects(){
 
         }`
     )
-=======
-import {createClient,groq} from "next-sanity";
 
-export async function getProjects(){
-
-    const client = createClient({
-        projectId: "yk2er0vp",
-        dataset:"production",
-       
-        apiVersion:"2023-09-22",
-        
-    }); 
-
-    return  client.fetch(
-
-        groq`*[_type = "project"]{
-            _id,
-            _createdAt,
-            date,
-            teams {
-                team1,
-                team2
-              },
-            prediction,
-            outcome,
-            matchstart
-
-
-        }`
-    )
->>>>>>> 205af39edd9b980b34dc296111e7cfc7eb057a24
 }
